bc00caf5d697154524ffe3e51dc9fb99cc6dca04
Fukuyama.Toshimichi <Fukuyama.Toshimichi@trans-cosmos.co.jp>
2024-01-24 02:16:12 +0900

    fixed html file 'v-for2' / js files 'notes, notes_from_b'

--- myapp/routes/notes_from_b.js
+++ myapp/routes/notes_from_b.js
@@ -1,26 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
-
+require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://xxxx:xxxx@cluster-cu01-wpjsfw.8glox1r.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
 router.use(cors());
 
 router.get('/', async (req, res) => {
-// データベース、コレクションを指定
-const database = client.db('notes');
-const notes = database.collection('notes');
-
-
-// 全てのドキュメントを取得
-const note = await notes.find({}).toArray();
-
-res.json(note);
+  // データベース、コレクションを指定
+  const database = client.db('notes');
+  const notes = database.collection('notes');
+  // 全てのドキュメントを取得
+  const note = await notes.find({}).toArray();
+  res.json(note);
 })
 
 module.exports = router;
